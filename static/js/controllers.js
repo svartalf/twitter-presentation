03f246b6be0d8588bf11ccca1a7545d63a905a39
SvartalF <self@svartalf.info>
2012-05-16 20:21:09 +0900

    Ability to pause a tweets stream

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,17 +2,31 @@
 
 var Stream = Spine.Controller.sub({
 
+    el: $('body'),
+
+    elements: {
+        '#tweets-stream': 'stream',
+        '#pause-overlay': 'overlay',
+        '#pause-overlay h3': 'counter'
+    },
+
+    events: {
+        'keypress': 'pause'
+    },
+
     _url: null,
 
-    el: $('#tweets-stream'),
+    // el: $('#tweets-stream'),
 
     queue: [],
+
+    interval_id: null,
 
     init: function(url) {
         this._url = url;
 
         Tweet.bind('create', this.proxy(this.create));
-        window.setInterval(this.proxy(this.show), 5000);
+        this.interval_id = window.setInterval(this.proxy(this.show), 5000);
 
         this._connect();
     },
@@ -36,6 +50,7 @@ var Stream = Spine.Controller.sub({
 
     create: function(object, event) {
         this.queue.push(object);
+        this.__update_counter();
     },
 
     show: function() {
@@ -43,12 +58,50 @@ var Stream = Spine.Controller.sub({
         if (!object)
             return;
 
-        return this.el.prepend(ich.tweet({
+        this.__update_counter();
+
+        return this.stream.prepend(ich.tweet({
             id: object.id,
             user: object.user().name,
             profile_image_url: object.user().image(),
             text: object.html()
         }));
+    },
+
+    pause: function() {
+        if (this.interval_id) {
+            window.clearInterval(this.interval_id);
+            this.interval_id = null;
+            this.overlay.show();
+        } else {
+            this.interval_id = window.setInterval(this.proxy(this.show), 5000);
+            this.overlay.hide();
+            this.show();
+        }
+    },
+
+    __update_counter: function() {
+        this.counter.html('+'+this.queue.length+' '+this.__pluralize(this.queue.length, 'твит', 'твита', 'твитов'));
+    },
+
+    __pluralize: function(value, str1, str2, str3) {
+        function plural(a) {
+            if ( a % 10 == 1 && a % 100 != 11 )
+                return 0;
+            else if ( a % 10 >= 2 && a % 10 <= 4 && ( a % 100 < 10 || a % 100 >= 20))
+                return 1;
+            else
+                return 2;
+        }
+
+        switch (plural(value)) {
+            case 0:
+                return str1;
+            case 1:
+                return str2;
+            default:
+                return str3;
+        }
     }
 
 });
